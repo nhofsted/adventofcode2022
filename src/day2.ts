09e52a61d5deaeecbdf54f31442be50727d8d76f
Nick Hofstede <nick.hofstede@gmail.com>
2022-12-03 00:09:09 +0100

    solution for day 2, part 2

--- src/day2.ts
+++ src/day2.ts
@@ -2,14 +2,22 @@ import fs = require('fs');
 import readline = require('readline');
 
 function winScore(opponent: number, my: number): number {
-    return ((my-opponent+4)%3)*3;
+    return ((my - opponent + 4) % 3) * 3;
 }
 
 function shapeScore(my: number): number {
     return my + 1;
 }
 
-async function part1(path: string) {
+function playForResult(opponent: number, result: number): number {
+    return (opponent + result + 2) % 3;
+}
+
+function superDuperObfuscated(opponent: number, result: number): number {
+    return 1 + (opponent + result + 2) % 3 + result * 3;
+}
+
+async function part2(path: string) {
     const fileStream = fs.createReadStream(path);
 
     const rl = readline.createInterface({
@@ -19,15 +27,30 @@ async function part1(path: string) {
     const A = 'A'.charCodeAt(0);
     const X = 'X'.charCodeAt(0);
 
-    var score = 0;
+    var score1 = 0;
+    var score2 = 0;
+    var score2b = 0;
     for await (const line of rl) {
         let opponentShape = line.charCodeAt(0) - A;
-        let myShape = line.charCodeAt(2) - X;
-        score += winScore(opponentShape, myShape);
-        score += shapeScore(myShape);
+        let result = line.charCodeAt(2) - X;
+
+        // calculate part 1 for old times' sake
+        let myShape1 = result;
+        score1 += winScore(opponentShape, myShape1);
+        score1 += shapeScore(myShape1);
+
+        // calculate part 2 in a readable way
+        let myShape2 = playForResult(opponentShape, result)
+        score2 += winScore(opponentShape, myShape2);
+        score2 += shapeScore(myShape2);
+
+        // calculate part 2 again, but now collapse everything in a giant obfuscated, but maybe more efficient, mess
+        score2b += superDuperObfuscated(opponentShape, result);
     }
 
-    console.log('Total score: ' + score);
+    console.log('Total score (part 1): ' + score1);
+    console.log('Total score (part 2, readable): ' + score2);
+    console.log('Total score (part 2, shortcut): ' + score2b);
 }
 
-part1('data/day2.txt');
+part2('data/day2.txt');
