3e3517afe20f7d87e46637760cfd1c9471a85e0c
Nick Hofstede <nick.hofstede@gmail.com>
2022-12-03 19:55:47 +0100

    solution for day 3, part 2

--- src/day3.ts
+++ src/day3.ts
@@ -2,6 +2,8 @@ import assert = require('assert');
 import fs = require('fs');
 import readline = require('readline');
 
+const priorityMapping = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
 async function part1(path: string) {
     const fileStream = fs.createReadStream(path);
 
@@ -9,17 +11,44 @@ async function part1(path: string) {
         input: fileStream,
     });
 
-    const priorityMapping = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
     let prioritySum = 0;
     for await (const line of rl) {
         const length = line.length;
-        const match = line.match('[' + line.slice(length/2) + ']');
+        const match = line.match('[' + line.slice(length / 2) + ']');
         assert(match != null);
-        prioritySum += priorityMapping.indexOf(match[0])+1;
+        prioritySum += priorityMapping.indexOf(match[0]) + 1;
     }
 
-    console.log('Sum of all priorities: ' + prioritySum);
+    console.log('Sum of all priorities (part 1): ' + prioritySum);
+}
+
+async function part2(path: string) {
+    const fileStream = fs.createReadStream(path);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+    });
+
+    let prioritySum = 0;
+    let lineCount = 1;
+
+    let charCount: number[] = new Array(52);
+    charCount.fill(0);
+
+    for await (const line of rl) {
+        for (var i = line.length; i-- > 0;) {
+            charCount[priorityMapping.indexOf(line.charAt(i))] |= lineCount;
+        }
+        lineCount <<= 1;
+        if (lineCount == 8) {
+            prioritySum += charCount.indexOf(7) + 1;
+            charCount.fill(0);
+            lineCount = 1;
+        }
+    }
+
+    console.log('Sum of all priorities (part 2): ' + prioritySum);
 }
 
 part1('data/day3.txt');
+part2('data/day3.txt');
