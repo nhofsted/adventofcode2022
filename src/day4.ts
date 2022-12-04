7edaef11c18645b68e7fdf8716b1a897d5f77328
Nick Hofstede <nick.hofstede@gmail.com>
2022-12-04 15:47:47 +0100

    solution for day 4, part 2

--- src/day4.ts
+++ src/day4.ts
@@ -1,20 +1,23 @@
 import fs = require('fs');
 import readline = require('readline');
 
-async function part1(path: string) {
+async function part2(path: string) {
     const fileStream = fs.createReadStream(path);
 
     const rl = readline.createInterface({
         input: fileStream,
     });
 
-    let sum = 0;
+    let contains = 0;
+    let overlap = 0;
     for await (const line of rl) {
         const ranges = line.split(/[-,]/).map(x => Number.parseInt(x));
-        sum += ranges[0] <= ranges[2] && ranges[1] >= ranges[3] || ranges[0] >= ranges[2] && ranges[1] <= ranges[3] ? 1 : 0;
+        contains += ranges[0] <= ranges[2] && ranges[1] >= ranges[3] || ranges[0] >= ranges[2] && ranges[1] <= ranges[3] ? 1 : 0;
+        overlap += ranges[0] <= ranges[2] && ranges[1] >= ranges[2] || ranges[0] <= ranges[3] && ranges[1] >= ranges[3] || ranges[0] >= ranges[2] && ranges[1] <= ranges[3] ? 1 : 0;
     }
 
-    console.log('In ' + sum + ' assignment pairs does one range fully contain the other');
+    console.log('In ' + contains + ' assignment pairs does one range fully contain the other');
+    console.log('In ' + overlap + ' assignment pairs does one range overlap the other');
 }
 
-part1('data/day4.txt');
+part2('data/day4.txt');
