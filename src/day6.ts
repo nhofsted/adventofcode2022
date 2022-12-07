195c128e321e2386b36acb9fdfd8d1c57e30d26e
Nick Hofstede <nick.hofstede@gmail.com>
2022-12-07 20:33:51 +0100

    day 6, alternate algorithm

--- src/day6.ts
+++ src/day6.ts
@@ -1,3 +1,4 @@
+import { assert } from 'console';
 import fs = require('fs');
 import readline = require('readline');
 
@@ -36,9 +37,79 @@ async function part2(path: string, size: number) {
     }
 }
 
+async function part2b(path: string, windowSize: number) {
+    const fileStream = fs.createReadStream(path);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+    });
+
+    // Create window state
+    const charCount: number[] = new Array(255);         // How many times does each character appear in the window?
+
+    line: for await (const line of rl) {
+        // Is the line long enough to contain the window?
+        if (line.length < windowSize) return;
+
+        charCount.fill(0);                              // Initially the window is empty
+        let neededUniqueChars = windowSize;             // How many unique characters do we still need?
+
+        // Fill up the window (unrolled the main loop for the start to avoid checking whether the start of the window is < 0)
+        for (let i = 0; i < windowSize; ++i) {
+            // Only characters that appear exactly once count towards our goal
+            switch (++charCount[line.charCodeAt(i)]) {
+                case 1:
+                    neededUniqueChars--;
+                    break;
+                case 2:
+                    neededUniqueChars++;
+                    break;
+            }
+        }
+        // Do we have windowSize amount of unique characters?
+        if (!neededUniqueChars) {
+            console.log(windowSize);
+            continue;
+        }
+
+        // Move window
+        for (let i = windowSize; i < line.length; ++i) {
+            // Old char drops out
+            switch (--charCount[line.charCodeAt(i - windowSize)]) {
+                case 0:
+                    neededUniqueChars++;
+                    break;
+                case 1:
+                    neededUniqueChars--;
+                    break;
+            }
+            // New char enters
+            switch (++charCount[line.charCodeAt(i)]) {
+                case 1:
+                    neededUniqueChars--;
+                    break;
+                case 2:
+                    neededUniqueChars++;
+                    break;
+            }
+            // Do we have windowSize amount of unique characters?
+            if (!neededUniqueChars) {
+                console.log(i + 1);
+                continue line;
+            }
+        }
+    }
+}
+
 async function parts() {
+    // part 1
     await part1('data/day6.txt');
+    // part 2 (first algorithm)
+    await part2('data/day6.txt', 4);
     await part2('data/day6.txt', 14);
+    // part 2 (second algorithm)
+    await part2b('data/day6.txt', 4);
+    await part2b('data/day6.txt', 14);
 }
 
 parts();
